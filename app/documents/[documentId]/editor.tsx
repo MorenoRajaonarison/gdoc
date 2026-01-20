cffd89ae15f414a17c2c83dde62ab1a76b287d9f
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2026-01-20 15:54:32 +0300

    wip rulers

--- app/documents/[documentId]/editor.tsx
+++ app/documents/[documentId]/editor.tsx
@@ -10,6 +10,7 @@ import { TaskList, TaskItem } from "@tiptap/extension-list";
 import { TableKit } from "@tiptap/extension-table";
 import { FontSize, TextStyleKit, LineHeight  } from "@tiptap/extension-text-style";
 import { useEditorStore } from "@/store/use-editor-store";
+import Ruler from "./ruler";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -97,6 +98,7 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#fafbfd] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
