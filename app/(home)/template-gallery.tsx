9175b37c1d58d866af4a29ee96f92e640b54e2e5
Moreno <rajaonarison.moreno@gmail.com>
2026-01-29 20:35:37 +0300

    wip: templates

---
+++ app/(home)/template-gallery.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
+
+const templates = [
+  {
+    id: "blank",
+    label: "Blank document",
+    imgUrl: "/logo.svg",
+  },
+];
+
+const TemplateGallery = () => {
+  const isCreating = false;
+  return (
+    <div className="bg-[#f1f3f4]">
+      <div className="max-w-7-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
+        <h3 className="text-base font-medium">Start a new Document</h3>
+        <Carousel>
+          <CarouselContent className="-ml-4">
+            {templates.map((template) => (
+              <CarouselItem
+                key={template.id}
+                className="basis-0.5 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 2xl:basis-[17.285714%] pl-4"
+              >
+                <div
+                  className={cn(
+                    "aspect-3/4 flex flex-col gap-y-2.5",
+                    isCreating && "pointer-events-none opacity-50",
+                  )}
+                >
+                  <button
+                    onClick={() => {}}
+                    disabled={isCreating}
+                    className="size-full hover:border-blue-500 rounded-sm border hover:bg-blue-50 transition flex flex-col items-center justify-center gap-y-4 bg-white"
+                    style={{
+                      backgroundImage: `url(${template.imgUrl})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      backgroundRepeat: "no-repeat",
+                    }}
+                  />
+                  <p className="text-sm font-medium truncate">
+                    {template.label}
+                  </p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default TemplateGallery;
